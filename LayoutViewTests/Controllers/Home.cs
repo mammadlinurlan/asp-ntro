6921aae892271d3f54253ec1a269e64d03854514
mammadlinurlan <nurlanym@code.edu.az>
2021-12-19 16:32:24 +0400

    commit1

---
+++ LayoutViewTests/Controllers/Home.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewTests.Controllers
+{
+    public class Home:Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
